80534eab254336ab9d411c835c5ece9a02e2913a
Lindsey Menges <lindseymenges@gmail.com>
2014-06-16 13:16:51 -0700

    changes font weight for big benefit statement, changes input fields to revert to normal styling if valid. TODO: fix hidden elements bug

--- javascripts/application.js
+++ javascripts/application.js
@@ -47,13 +47,17 @@ View.prototype = {
 
 var highlightFields = function(){
     for(x = 0; x < document.getElementsByTagName('input').length; x++){
+      var inputField = document.getElementsByTagName('input').item(x)
+      var errorMessage = document.getElementById('sign_up_error_message')
       if(document.getElementsByTagName('input').item(x).value == ''){
-        var inputField = document.getElementsByTagName('input').item(x)
-        var errorMessage = document.getElementById('sign_up_error_message')
         inputField.style.border = "2px solid #efc7c7"
         inputField.style.backgroundColor = "#f9eded";
         errorMessage.style.display = "block"
         return false
+       }
+       else{
+        inputField.style.border = "1px solid #d1d1d0"
+        inputField.style.backgroundColor = "#fefff1"
        }
      }
      return true;
