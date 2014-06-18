e00b08e0892d82b87bd036cde79dee288649597f
Lindsey Menges <lindseymenges@gmail.com>
2014-06-17 22:48:36 -0700

    finishing touches

--- javascripts/application.js
+++ javascripts/application.js
@@ -12,10 +12,27 @@ Controller.prototype = {
   bindEvents: function(){
     displaySignUpButton = document.getElementById('display_sign_up_button')
     displaySignUpButton.onclick = view.displaySignUpInfo;
+  },
+
+  highlightFields: function(){
+    var errorMessage = document.getElementById('sign_up_error_message')
+    for(x = 0; x < document.getElementsByTagName('input').length; x++){
+      var inputField = document.getElementsByTagName('input').item(x)
+      if(inputField.value == ''){
+        view.markInputAsIncorrect(inputField);
+        view.errorMessageDisplay(errorMessage);
+        return false
+       }
+       else{
+        view.removeIncorrectClass(inputField)
+       }
+     }
+     return true;
   }
 }
 
 View = function(){
+  
 };
 
 View.prototype = {
@@ -24,33 +41,17 @@ View.prototype = {
     var displaySignUpButton = document.getElementById('display_sign_up_button')
       signUpInformation.style.display = "inline-block"
       displaySignUpButton.remove();
+  },
+
+  markInputAsIncorrect: function(element){
+    element.className += " incorrect_input"
+  },
+
+  removeIncorrectClass: function(element){
+    element.className = element.className.replace(/\bincorrect_input\b/g , '' )
+  },
+
+  errorMessageDisplay: function(errorMessage){
+    errorMessage.className = errorMessage.className.replace(/\binvisible\b/, 'visible')
   }
-}
-
-var highlightFields = function(){
-    for(x = 0; x < document.getElementsByTagName('input').length; x++){
-      var inputField = document.getElementsByTagName('input').item(x)
-      var errorMessage = document.getElementById('sign_up_error_message')
-      if(inputField.value == ''){
-        markInputAsIncorrect(inputField);
-        errorMessageDisplay(errorMessage);
-        return false
-       }
-       else{
-        removeIncorrectClass(inputField)
-       }
-     }
-     return true;
-  }
-
-var markInputAsIncorrect = function(element) {
-  element.className += " incorrect_input"
-}
-
-var removeIncorrectClass = function(element){
-  element.className = element.className.replace(/\bincorrect_input\b/g , '' )
-}
-
-var errorMessageDisplay = function(errorMessage){
-  errorMessage.className = errorMessage.className.replace(/\binvisible\b/, 'visible')
 }
