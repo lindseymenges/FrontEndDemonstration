e99a4df7123ef978a3af4ba06434cf8d138578e9
Lindsey Menges <lindseymenges@gmail.com>
2014-06-17 23:18:01 -0700

    pulls out javascripts into separate files.

--- javascripts/landing_page_controller.js
+++ javascripts/landing_page_controller.js
@@ -30,28 +30,3 @@ Controller.prototype = {
      return true;
   }
 }
-
-View = function(){
-  
-};
-
-View.prototype = {
-  displaySignUpInfo: function(){
-    var signUpInformation = document.getElementById('main_right')
-    var displaySignUpButton = document.getElementById('display_sign_up_button')
-      signUpInformation.style.display = "inline-block"
-      displaySignUpButton.remove();
-  },
-
-  markInputAsIncorrect: function(element){
-    element.className += " incorrect_input"
-  },
-
-  removeIncorrectClass: function(element){
-    element.className = element.className.replace(/\bincorrect_input\b/g , '' )
-  },
-
-  errorMessageDisplay: function(errorMessage){
-    errorMessage.className = errorMessage.className.replace(/\binvisible\b/, 'visible')
-  }
-}
