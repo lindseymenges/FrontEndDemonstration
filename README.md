23fcd16466f12bb4fa352a53df94aef49c991055
Lindsey Menges <lindseymenges@gmail.com>
2014-06-19 11:04:19 -0700

    updates README

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ FrontEndDemonstration
 =====================
 
 Website landing page and sign-up form mockup
+
+Given the following images:
+![Landing Page](images/Landing.jpg "Landing Page")
+![Landing Error State](images/Landing_error_state.jpg "Error State")
+
+I created a landing page for a mock website with an interactive sign-up form and responsive design for both desktop and mobile.
--- javascripts/landing_page_controller.js
+++ javascripts/landing_page_controller.js
@@ -16,17 +16,20 @@ Controller.prototype = {
 
   highlightFields: function(){
     var errorMessage = document.getElementById('sign_up_error_message')
+    errorArray = []
     for(x = 0; x < document.getElementsByTagName('input').length; x++){
       var inputField = document.getElementsByTagName('input').item(x)
       if(inputField.value == ''){
         view.markInputAsIncorrect(inputField);
         view.errorMessageDisplay(errorMessage);
         return false
+        // push "false" into errorArray
        }
        else{
         view.removeIncorrectClass(inputField)
        }
      }
+     // if errorArray[0] OR errorArray.contain "false" return false, otherwise return true
      return true;
   }
 }
